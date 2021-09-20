f7f8c2b2f185a3752eb23ca02bb1149eb65cd9e3
kishore <kishorekumar.r@techjays.com>
2021-09-20 11:27:30 +0530

    comparison changes done

--- app/src/main/java/com/techjays/chatapplication/MainActivity.kt
+++ app/src/main/java/com/techjays/chatapplication/MainActivity.kt
@@ -2,18 +2,10 @@ package com.techjays.chatapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.techjays.chatlibrary.ChatFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val homeFragment = ChatFragment.newInstance("test")
-        if(savedInstanceState == null) { // initial transaction should be wrapped like this
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.root_container, homeFragment)
-                .commitAllowingStateLoss()
-        }
-
     }
 }
--- chatlibrary/src/main/java/com/techjays/chatlibrary/ChatActivity.kt
+++ chatlibrary/src/main/java/com/techjays/chatlibrary/ChatActivity.kt
@@ -2,10 +2,6 @@ package com.techjays.chatlibrary
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
-import android.view.ViewGroup
-import android.view.LayoutInflater
-import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
