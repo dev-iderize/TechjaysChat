f0f61841c3e99329a503121b93321fd801fe5cb1
SrinathDev <nathsri258@gmail.com>
2021-09-23 14:40:47 +0530

    Main Activity added

--- app/src/main/java/com/techjays/chatapplication/MainActivity.kt
+++ app/src/main/java/com/techjays/chatapplication/MainActivity.kt
@@ -3,12 +3,10 @@ package com.techjays.chatapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import androidx.fragment.app.FragmentTransaction
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.techjays.chatlibrary.ChatListActivity
-import com.techjays.chatlibrary.ChatListFragment
+import com.techjays.chatlibrary.LibChatListActivity
+import com.techjays.chatlibrary.ChatListFragmentLib
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,20 +22,20 @@ class MainActivity : AppCompatActivity() {
     private fun initView() {
         mButton = findViewById(R.id.nav_activity)
         mButton.setOnClickListener {
-            val intent = Intent(this@MainActivity, ChatListActivity::class.java)
+            val intent = Intent(this@MainActivity, LibChatListActivity::class.java)
             intent.putExtra("base_url", "https://sprint.myvidhire.com/api/v1/")
             intent.putExtra(
                 "chat_token",
-                "gAAAAABhSyIaKLQbYN0-yjrNh_es5hcGSQI0HTUCi8Z-slMnrDxs7vg6OB3YUOwzsocVYyezKTjV0GPHd8kuyOqgniQmy6iaAljrOvYwUS00IgRv4EPoVt_nO3i5e3lzdf2A5W52GYrxE1ps63t-d_oUPKwjqRYxBQ=="
+                "gAAAAABhSyKfrKoBKtN_SDmfp_hHmMdxU3igV3dc99h9RcVJaVHC57N2VhYNptZmzyTV8CUh09mDJvPcr4cWVekeRBwC9o9gQSFIV2ZB9NeURjIgIRvkEfZwwIF3fEpmq2a8rs2rSIRCWzCIX1g6YJOlbA5Gsu_UbQ=="
             )
-            intent.putExtra("auth_token", "1d0e5734f76ad754333a3b297442a4b1f38eb60e")
+            intent.putExtra("auth_token", "5cee957a8fe5b72393d2511818c33eef5fd18d1c")
             startActivity(intent)
         }
 
         mButton2 = findViewById(R.id.nav_fragment)
         mButton2.setOnClickListener {
 
-            val newFragment = ChatListFragment.newInstance(
+            val newFragment = ChatListFragmentLib.newInstance(
                 "https://sprint.myvidhire.com/api/v1/",
                 "gAAAAABhSxSGLih9x9FtS2q2LjW4IsGn_oFDpGIyrsOY0qygwPRb-h0R6BSxtZ3AT0qmVHzHVZ2X--z0R9k1BRK5OYdvRnhYDH0tmK0CT2w9rNBKS6-b38MWvTrvPjzlkB8IxuJq7cMPyVF3KuoEiLvBfYs6IpuJzg==",
                 "1d0e5734f76ad754333a3b297442a4b1f38eb60e"
