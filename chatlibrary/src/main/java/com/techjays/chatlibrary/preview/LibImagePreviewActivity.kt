4e2a90392d2b1d02c8989d7044ebd93b36195f66
Mathan <mathankumar.g@iderize.in>
2022-04-12 20:27:22 +0530

    Zoom imageview

--- chatlibrary/src/main/java/com/techjays/chatlibrary/preview/LibImagePreviewActivity.kt
+++ chatlibrary/src/main/java/com/techjays/chatlibrary/preview/LibImagePreviewActivity.kt
@@ -1,31 +1,26 @@
 package com.techjays.chatlibrary.preview
 
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.techjays.chatlibrary.R
 import com.techjays.chatlibrary.util.Utility
+import kotlinx.android.synthetic.main.activity_lib_image_preview.*
 
 class LibImagePreviewActivity : AppCompatActivity() {
-
-    lateinit var mPhoto: ImageView
-    lateinit var mBack:ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lib_image_preview)
-        val data = intent
-        var mUrl = data.getStringExtra("url_data")
 
-        mPhoto = findViewById(R.id.photo_view)
-        mBack = findViewById(R.id.libImgBack)
+        val data = intent
+        val mUrl = data.getStringExtra("url_data")
 
         Utility.loadUserImage(
-            mUrl, mPhoto,
+            mUrl, photo_view,
             this
         )
 
-        mBack.setOnClickListener {
+        libImgBack.setOnClickListener {
             onBackPressed()
         }
     }
