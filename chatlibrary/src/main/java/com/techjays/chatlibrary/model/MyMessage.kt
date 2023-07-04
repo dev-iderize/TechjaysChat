cf5c771da736d45fa3e742d5f702f0a0df212259
sharonpjoseph <sharon.joseph@techjays.com>
2023-07-04 08:41:52 +0530

    phone number

--- chatlibrary/src/main/java/com/techjays/chatlibrary/model/MyMessage.kt
+++ chatlibrary/src/main/java/com/techjays/chatlibrary/model/MyMessage.kt
@@ -13,6 +13,7 @@ data class MyMessage(
         @SerializedName("group_id") val groupId: Int,
         @SerializedName("message_id") val messageId: Int,
         @SerializedName("message_type") val messageType: String,
+        @SerializedName("phone_number") var mPhoneNumber: String = "",
         @SerializedName("message") val message: String,
         @SerializedName("file_type") val fileType: String,
         @SerializedName("medium_image") val mediumImage: String,
